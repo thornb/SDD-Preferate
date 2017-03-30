cd1a4d9b0d2400c1a8775a44ff538919fb4bd92d
thornb <bthorne3@gmail.com>
2017-03-29 20:53:32 -0400

    Test conecting to database

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,13 +7,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 
 
--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -3,18 +3,44 @@ package hello;
 public class Greeting {
 
     private final long id;
-    private final String content;
+    //private final String content;
 
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
+    public Greeting() {
+
+        String url = "jdbc:mysql://localhost:3306/preferate";
+        String username = "root";
+        String password = "CrackerWindow654";
+
+        System.out.println("Connecting database...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            Statement stmt = connection.createStatement();
+            ResultSet stmt.executeQuery("SELECT * FROM `user`");
+
+            while(rs.next()){
+                System.out .println(rs.getInt("id"));
+            }
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+
+
+        //this.id = id;
+        // this.content = content;
     }
 
     public long getId() {
         return id;
     }
 
-    public String getContent() {
-        return content;
-    }
+    // public String getContent() {
+    //     return content;
+    // }
 }
