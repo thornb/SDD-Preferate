ff145c9a3edc985e0327f89e18271884e1613ac6
thornb <bthorne3@gmail.com>
2017-04-20 00:50:07 -0400

    database call for restaurant name

--- src/main/java/restClasses/Recommendation.java
+++ src/main/java/restClasses/Recommendation.java
@@ -10,10 +10,41 @@ public class Recommendation {
 	private String restaurant_name;
 	private double rating;
 
-	public Recommendation(long restaurant_id, String, restaurant_name, double rating){
+	public Recommendation(long restaurant_id, double rating){
 		this.restaurant_id = restaurant_id;
-		this.restaurant_name = restaurant_name;
 		this.rating = rating;
+
+		//connect to DB to get restaurant name
+		//Parameters to log into database
+        String url = "jdbc:mysql://localhost:3306/preferate";
+        String username = "root";
+        String password = "CrackerWindow654";
+
+        System.out.println("Connecting database...");
+
+        //Try to connect to the database
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+
+            Statement stmt = connection.createStatement();
+            String query = "SELECT restaurant_name FROM restaurants WHERE restaurant_id =" + this.restaurant_id;
+            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            ResultSet rs = preparedStmt.executeQuery();
+
+            if(rs.next()){
+
+            	this.restaurant_name = rs.getString("restaurant_name");
+
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+
+
 	}
 
 
